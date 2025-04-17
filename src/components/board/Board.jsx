67858ed64c426879471c93c0bc54754f081962e4
emba2ra3star <tachi.i.061@gmail.com>
2025-04-17 16:30:10 +0800

    刪掉BoardTodoList.jsx

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router";
 
 import AddTodayItem from "./AddTodayItem";
 import BoardPomodoro from "./BoardPomodoro";
-import BoardTodoList from "./BoardTodoList";
 import TodayList from "./TodayList";
 
 function Board() {
@@ -37,5 +36,22 @@ function Pond() {
                 <p>魚塘</p>
             </Link>
         </>
+    );
+}
+
+function BoardTodoList() {
+    return (
+        <Link to={"/TodoList"} className="h-[100%] px-[2rem] py-[1rem] bg-base-100 border-1 rounded-lg border-base-content">
+            <span>代辦事項</span>
+            <section className="grid gap-[1rem]">
+                <div>
+                    <div className="flex flex-row gap-[1rem]">
+                        <p>3/28</p>
+                        <p>完成英文雜誌文章學習一篇</p>
+                    </div>
+                    <div className="divider my-0"></div>
+                </div>
+            </section>
+        </Link>
     );
 }
