4b4ff933f52819601d91a20c3e5a6c790f446624
emba2ra3star <tachi.i.061@gmail.com>
2025-04-17 16:32:15 +0800

    刪掉AddTodayItem.jsx

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router";
 
-import AddTodayItem from "./AddTodayItem";
 import BoardPomodoro from "./BoardPomodoro";
 import TodayList from "./TodayList";
 
@@ -35,6 +34,32 @@ function Pond() {
             <Link to="/fishgame" className="w-[30%] px-[2rem] py-[1rem] flex justify-center items-center bg-base-100 border-1 rounded-lg border-base-content">
                 <p>魚塘</p>
             </Link>
+        </>
+    );
+}
+
+function AddTodayItem() {
+    return (
+        <>
+            <div className="flex flex-row h-fit px-[2rem] py-[1rem] items-center justify-between gap-[1rem] bg-base-100  border-1 rounded-lg border-base-content">
+                {/* 送出按鈕 */}
+                <input type="checkbox" className="mt-[1.5rem] checkbox border-base-content bg-base-100 checked:bg-base-content checked:text-base-100 checked:border-base-100 " />
+                {/* 事項名稱 */}
+                <div className="w-[100%] flex flex-col">
+                    <span>事項名稱</span>
+                    <input type="text" placeholder="輸入工作項目" className="input border-1 rounded-lg border-base-content" />
+                </div>
+                {/* 開始時間 */}
+                <div className="w-[16rem]">
+                    <span>開始時間</span>
+                    <input type="time" className="input border-1 rounded-lg border-base-content"/>
+                </div>
+                {/* 結束時間 */}
+                <div className="w-[16rem]">
+                    <span>結束時間</span>
+                    <input type="time" className="input border-1 rounded-lg border-base-content"/>
+                </div>
+            </div>
         </>
     );
 }
