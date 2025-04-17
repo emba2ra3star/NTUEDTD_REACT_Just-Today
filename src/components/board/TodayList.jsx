f54de73433c5700b5acb6d7e6a96dcd118d7b5c5
emba2ra3star <tachi.i.061@gmail.com>
2025-04-17 23:45:26 +0800

    首頁RWD

--- src/components/board/TodayList.jsx
+++ src/components/board/TodayList.jsx
@@ -1,16 +1,16 @@
 function TodayList({ data }) {
     return (
-        <div className="w-2/5 bg-base-100 px-[2rem] py-[1rem] border-1 rounded-lg border-base-content h-full">
+        <div className="lg:w-2/5 bg-base-100 px-[2rem] py-[1rem] border-1 rounded-lg border-base-content h-full">
             
-            <span className="text-[2rem]">今日行程</span>
+            <span className="text-[20px] sm:text-[2rem]">今日行程</span>
             
             <section className="grid gap-[1rem] list mt-[1rem]">
 
-                {data ? data.map((task, index) => (
-                    <div key={index} className="list-row py-2 px-6 border-1 rounded-lg border-base-content">
-                        <div className="flex flex-row gap-[0.5rem] list-col-grow items-center">
-                            <div aria-label="status" className="status bg-[#8FE189]"></div>
-                            <span className="">{task.startTime}</span>
+                {data&&data.length > 0 ? data.map((task, index) => (
+                    <div key={index} className="list-row py-2 px-6 items-center text-[14px] md:text-[1rem] border-1 rounded-lg border-base-content">
+                        <div className="flex flex-row flex-wrap gap-[0.5rem] list-col-grow items-center">
+                            <div aria-label="status" className="hidden sm:block status bg-[#8FE189]"></div>
+                            <span className="text-wrap">{task.startTime}</span>
                             <p>{task.title}</p>
                         </div>
                         <input type="checkbox" className="checkbox border-base-content bg-base-100 checked:bg-base-content checked:text-base-100 checked:border-base-100 "/>
