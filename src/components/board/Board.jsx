88981a41f5d84b652c45408b9ff3beda99702e64
emba2ra3star <tachi.i.061@gmail.com>
2025-04-17 17:30:43 +0800

    AddTodayItem元件修正

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -12,29 +12,7 @@ function Board() {
                 {/* 右欄 */}
                 <div className="w-3/5 flex flex-col gap-[1rem]">
                     {/* 輸入工作 */}
-
-                    <div className="list bg-base-100  border-1 rounded-lg border-base-content">
-                        <div className="list-row">
-                            {/* 送出按鈕 */}
-                            <input type="checkbox" className="mt-[1.5rem] checkbox border-base-content bg-base-100 checked:bg-base-content checked:text-base-100 checked:border-base-100 " />
-                            {/* 事項名稱 */}
-                            <div className="list-col-grow min-w-[70px] flex flex-col">
-                                <span>事項名稱</span>
-                                <input type="text" placeholder="輸入工作項目" className="input w-full border-1 rounded-lg border-base-content" />
-                            </div>
-                            {/* 開始時間 */}
-                            <div className="w-[115px]">
-                                <span>開始時間</span>
-                                <input type="time" className="input border-1 rounded-lg border-base-content text-[14px]" />
-                            </div>
-                            {/* 結束時間 */}
-                            <div className="w-[115px]">
-                                <span>結束時間</span>
-                                <input type="time" className="input border-1 rounded-lg border-base-content text-[14px]" />
-                            </div>
-                        </div>
-                    </div>
-
+                    <AddTodayItem />
                     {/* 魚塘與番茄鐘 */}
                     <div className="flex flex-row gap-[1rem] h-fit">
                         <Pond />
@@ -52,27 +30,27 @@ export default Board;
 
 function AddTodayItem() {
     return (
-        <>
-            <div className="flex flex-row h-fit px-[2rem] py-[1rem] items-center justify-between gap-[1rem] bg-base-100  border-1 rounded-lg border-base-content">
+        <div className="list bg-base-100  border-1 rounded-lg border-base-content">
+            <div className="list-row">
                 {/* 送出按鈕 */}
                 <input type="checkbox" className="mt-[1.5rem] checkbox border-base-content bg-base-100 checked:bg-base-content checked:text-base-100 checked:border-base-100 " />
                 {/* 事項名稱 */}
-                <div className="w-[100%] flex flex-col">
+                <div className="list-col-grow min-w-[70px] flex flex-col">
                     <span>事項名稱</span>
-                    <input type="text" placeholder="輸入工作項目" className="input border-1 rounded-lg border-base-content" />
+                    <input type="text" placeholder="輸入工作項目" className="input w-full border-1 rounded-lg border-base-content" />
                 </div>
                 {/* 開始時間 */}
-                <div className="w-[16rem]">
+                <div className="w-[115px]">
                     <span>開始時間</span>
-                    <input type="time" className="input border-1 rounded-lg border-base-content" />
+                    <input type="time" className="input border-1 rounded-lg border-base-content text-[14px]" />
                 </div>
                 {/* 結束時間 */}
-                <div className="w-[16rem]">
+                <div className="w-[115px]">
                     <span>結束時間</span>
-                    <input type="time" className="input border-1 rounded-lg border-base-content" />
+                    <input type="time" className="input border-1 rounded-lg border-base-content text-[14px]" />
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
