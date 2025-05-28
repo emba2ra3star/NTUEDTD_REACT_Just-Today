14557c259db0a2073e006913be3a6a2bbbb9bdee
emba2ra3star <tachi.i.061@gmail.com>
2025-05-28 15:38:40 +0800

    NavMenu(調整高度+當前選項)

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 function NavMenu() {
-    const pageTitle = "番茄鐘";
+    const pageTitle = useLocation();
     const navList = [
         { name: "今日事項", path: "/" },
         { name: "待辦事項", path: "/TodoList" },
@@ -12,13 +12,13 @@ function NavMenu() {
     ]
 
     return (
-        <div className="w-1/7 min-w-[210px] h-full pb-11 flex flex-col justify-between">
+        <div className="w-1/7 min-w-[210px] h-[calc(screen-5rem)] pb-11 flex flex-col justify-between">
             <div className="flex flex-col gap-2">
                 {navList.slice(0, 4).map((item, index) => (
                     <Link
                         to={item.path}
                         key={index}
-                        className={`pl-[92px] py-2 pr-4 rounded-r-lg w-full ${pageTitle == item.name ? "bg-gray-400" : "bg-white"} hover:bg-gray-300 text-2xl`}
+                        className={`pl-[92px] py-2 pr-4 rounded-r-lg w-full ${pageTitle.pathname == item.path ? "bg-gray-400" : "bg-white"} hover:bg-gray-300 text-2xl`}
                     >
                         {item.name}
                     </Link>
@@ -30,7 +30,7 @@ function NavMenu() {
                     <Link
                         to={item.path}
                         key={index}
-                        className={`pl-[92px] py-2 pr-4 rounded-r-lg w-full ${pageTitle == item.name ? "bg-gray-400" : "bg-white"} hover:bg-gray-300 text-2xl`}
+                        className={`pl-[92px] py-2 pr-4 rounded-r-lg w-full ${pageTitle.pathname == item.path ? "bg-gray-400" : "bg-white"} hover:bg-gray-300 text-2xl`}
                     >
                         {item.name}
                     </Link>
