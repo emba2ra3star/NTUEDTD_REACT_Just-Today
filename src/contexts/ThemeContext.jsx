709af7a2583ff2b6eaaf9dea5e960436496a6229
meminn422 <fsisi0609@gmail.com>
2025-06-06 01:38:07 +0800

    add themecontext

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "淺色");
+
+  useEffect(() => {
+    const isDark = theme === "深色";
+    document.documentElement.classList.toggle("dark", isDark);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
